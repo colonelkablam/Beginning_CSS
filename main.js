31902b3bc7298333ac76dba76bf34c0b2b016c01
Nick Harding <100789789+colonelkablam@users.noreply.github.com>
2024-11-04 15:21:19 +0000

    added CSS selectors page with small JS button

---
+++ main.js
@@ -0,0 +1,6 @@
+function displayResult() {
+    const result = "Hello, welcome to the " + document.title + " page!";
+        
+    // Display result in the div with id 'output'
+    document.getElementById("display-function-output").textContent = result;
+}
